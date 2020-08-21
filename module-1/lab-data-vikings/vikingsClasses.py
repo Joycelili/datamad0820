308547a2bf6609e2ef0c0b856b88f075b0182584
Joycelili <joycelili@hotmail.com>
2020-08-21 18:11:02 +0200

    Empieza la batalla

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -3,18 +3,30 @@
 
 
 class Soldier:
-    pass
+    def __init__ (self,health, strength):
+    self. health = health
+    self.strength = strength
+
+def attack (self):
+    self.attack = 0
+    return strength   
+
+def receiveDamage (self):
+    self.receiveDamage = health - 1
+     
 
 # Viking
 
 
 class Viking:
+    def _int_ (self):
     pass
 
 # Saxon
 
 
 class Saxon:
+    def _int_(self)
     pass
 
 # War
