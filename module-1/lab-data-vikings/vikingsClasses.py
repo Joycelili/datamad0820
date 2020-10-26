c8e4106a22a7f32484f4ab7c16f1ba2e40835cf0
Joycelili <joycelili@hotmail.com>
2020-10-26 22:05:43 +0100

    mejora sin bonus :(

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -7,24 +7,23 @@ class Soldier:
         self.health = health
         self.strength = strength
 
-    def attack (self):
+    def attack(self):
         return self.strength   
 
-    def receiveDamage (self, damage):
+    def receiveDamage(self, damage):
         self.health -= damage
  
 #Viking
 
 class Viking(Soldier):
-    def __init__ (self, name, health, strength):
-        self.name = name 
-        self.health = health
-        self.strength = strength 
-
+    def __init__(self, name, health, strength):
+        super().__init__(health,strength)
+        self.name = name
+    
     def attack (self): # En este caso estaría sobreescribiendo?
         return self.strength
 
-    def receiveDamage (self, damage):
+    def receiveDamage(self, damage):
         self.health -= damage 
         if self.health > 0:
             return self.name + " has received " + str(damage) + " points of damage"
@@ -32,14 +31,13 @@ class Viking(Soldier):
             return self.name + " has died in act of combat"
 
     def battleCry(self):
-         return "Odin Owns You All!" #tristeza que no deja pornerlo en Vikingo :(
+         return "Odin Owns You All!" 
 
 # Saxon
 
 class Saxon(Soldier):
-    def __init__ (self, health, strength):
-        self.health = health
-        self.strength = strength
+    def __init__(self,health,strength):
+        super().__init__(health,strength)
 
     def attack (self):
         return self.strength   
@@ -58,27 +56,27 @@ class War:
         self.vikingArmy = []
         self.saxonArmy = []
 
-    def addViking (self, vikingwarrior):
-        vi = vikingwarrior 
-        if isinstance(vi, vikingwarrior):
-            self.vikingArmy.append(vi)
+    def addViking (self, viking):
+        self.vikingArmy.append(viking)
 
-    def addSaxon (self, saxonwarrior):
-        sa = saxonwarrior
-        if isinstance(sa, saxonwarrior): 
-            self.saxonArmy.append(sa)
+    def addSaxon (self, saxon):
+        self.saxonArmy.append(saxon)
 
     def vikingAttack (self):
-        ramdonV:random.randint(0,1)
-        randomS: ramdon.randint(0,1)
-        if Saxon.receiveDamage == Viking.strength: #como llamar a los otros? con super(), el super no seria para la funcion mas cercana?
-            saxonArmy -= 1
-            return
+        sax = random.choice(self.saxonArmy)
+        vik = random.choice(self.vikingArmy)
+        combat= self.saxon.receiveDamage(self.vik.strength)
+        if self.vik.health <= 0:
+            vikingArmy.remove(self.vik)
+        return combat
 
     def saxonAttack (self):
-        if Viking.receiveDamage == Saxon.strength:
-            vikingArmy -= 1
-            return 
+        sax = random.choice(self.saxonArmy)
+        vik = random.choice(self.vikingArmy)
+        combat= self.vik.receiveDamage(self.sax.strength)
+        if self.sax.health <= 0:
+            saxonArmy.remove(self.sax)
+        return combat
 
     def showStatus (self):
         if len(self.saxonArmy) == 0:
@@ -87,4 +85,3 @@ class War:
             return "Saxons have fought for their lives and survive another day..."
         else:
             return "Vikings and Saxons are still in the thick of battle."
-#No pase la 4ta prueba y me salen todos los errores en la linea 68. No di con el error
